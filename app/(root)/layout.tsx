e1ce39744f655519f07becda18c51c823506ef6d
Gevalinho <gevalinho1@gmail.com>
2024-10-23 14:36:30 +0100

    Implementing sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,15 @@
-import { Sidebar } from "lucide-react";
+import Sidebar from "@/components/ui/Sidebar";
+// import "./globals.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggin = { firstName: "John", lastName: "Doe" };
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar />
+      <Sidebar user={loggin.firstName} />
       {children}
     </main>
   );
